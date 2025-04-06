f2d1beb7a208fa7d5a00ff9cabfe54c3f751a7a2
ram-25082005 <rampalla2005@gmail.com>
2025-04-06 16:14:39 +0530

    .

--- model_app.py
+++ model_app.py
@@ -1,11 +1,19 @@
 import pickle
+import numpy as np
+import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 with open("D:\\MACHINE LEARNING\\PROJECTS\\emotion detection\\emotion_model.pkl","rb") as f:
   model=pickle.load(f)
 with open("D:\\MACHINE LEARNING\\PROJECTS\\emotion detection\\vectorizer.pkl","rb") as f1:
   vectorizer=pickle.load(f1)
-input1=["I am so sad"]
+from tensorflow.keras.models import load_model
+
+neural_model=load_model("D:\\MACHINE LEARNING\\PROJECTS\\emotion detection\\neural_model.keras")
+neural_model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=["accuracy"])
+input1=["please dont kill me"]
 input_features=vectorizer.transform(input1)
-prediction=model.predict(input_features)
+prediction=neural_model.predict(input_features)
+prediction=np.argmax(prediction)
 if prediction==0:
   print("Anger")
 elif(prediction==1):
